929eaff6bdd0baa3fefc8e1aef6be2d79a9e3813
Ghanshyam pandey <44994843+famousscript@users.noreply.github.com>
2018-11-28 17:38:50 +0530

    Angular Material design
    Angular Material design

---
+++ README.md
@@ -0,0 +1,15 @@
+# Angular6 SPA
+
+For Direct run this project you need to follow these steps:
+
+Make sure you have installed node in properly
+
+1. Take all files and folder and create your own folder like "spa" folder name then. 
+2. Go to node prompt and "cd" to created folder directory then.
+3. Now type in this directory like /spa>npm install.
+4. Now /spa>ng serve
+5. then go to your browser and type http://localhost:4200/ you will see the homepage.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ src/app/app.module.ts
@@ -0,0 +1,113 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { MyTableComponent } from './my-table/my-table.component';
+import { AppRouterComponent } from './app.router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MyNavComponent,
+    DashboardComponent,
+    MyDashboardComponent,
+    MyTableComponent
+  ],
+  imports: [
+    AppRouterComponent,
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
